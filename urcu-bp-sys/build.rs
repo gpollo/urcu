ff6c7919899f7d57e3675984d27cbc04b9705942
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2024-09-27 20:45:53 -0400

    urcu-*-sys: Add 'static' feature flag

--- urcu-bp-sys/build.rs
+++ urcu-bp-sys/build.rs
@@ -1,13 +1,13 @@
 use std::path::PathBuf;
 
 fn main() {
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let build_config = urcu_src::build_config();
     metadeps::probe().unwrap();
 
-    let output = PathBuf::from(std::env::var("OUT_DIR").unwrap());
-
-    println!("cargo:rustc-link-lib=urcu-bp");
-    println!("cargo:rerun-if-changed=src/header.h");
-    bindgen::Builder::default()
+    build_config.cargo_link("urcu-bp");
+    build_config
+        .default_bindgen()
         .header("src/header.h")
         .blocklist_type("rcu_flavor_struct")
         .blocklist_type("rcu_head")
@@ -16,6 +16,8 @@ fn main() {
         .allowlist_function("urcu_bp_.*")
         .generate()
         .unwrap()
-        .write_to_file(output.join("bindings.rs"))
+        .write_to_file(out_dir.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+
+    println!("cargo:rerun-if-changed=src/header.h");
 }
--- urcu-sys/build.rs
+++ urcu-sys/build.rs
@@ -4,9 +4,9 @@ use bindgen::callbacks::{FieldInfo, ParseCallbacks};
 use bindgen::FieldVisibilityKind;
 
 #[derive(Debug)]
-struct CustomCallbacks;
+struct BindgenCallbacks;
 
-impl ParseCallbacks for CustomCallbacks {
+impl ParseCallbacks for BindgenCallbacks {
     fn field_visibility(&self, info: FieldInfo<'_>) -> Option<FieldVisibilityKind> {
         match info.type_name {
             "urcu_gp_poll_state" | "rcu_head" => Some(FieldVisibilityKind::Private),
@@ -16,30 +16,33 @@ impl ParseCallbacks for CustomCallbacks {
 }
 
 fn main() {
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let build_config = urcu_src::build_config();
     metadeps::probe().unwrap();
 
-    let output = PathBuf::from(std::env::var("OUT_DIR").unwrap());
-    let package = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
-
-    println!("cargo:rustc-link-lib=urcu");
-    println!("cargo:rerun-if-changed=src/header.h");
-    bindgen::Builder::default()
+    build_config.cargo_link("urcu-cds");
+    build_config.cargo_link("urcu");
+    build_config
+        .default_bindgen()
         .header("src/header.h")
         .allowlist_item("cds_.*")
+        .allowlist_item("__cds_.*")
         .allowlist_item("rcu_.*")
         .allowlist_item("urcu_gp_poll_state")
         .allowlist_var("CDS_.*")
-        .parse_callbacks(Box::new(CustomCallbacks))
+        .parse_callbacks(Box::new(BindgenCallbacks))
         .derive_default(true)
         .wrap_static_fns(true)
-        .wrap_static_fns_path(output.join("static_fns.c"))
+        .wrap_static_fns_path(out_dir.join("static_fns.c"))
         .generate()
         .unwrap()
-        .write_to_file(output.join("bindings.rs"))
+        .write_to_file(out_dir.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+    build_config
+        .default_cc()
+        .include(env!("CARGO_MANIFEST_DIR"))
+        .file(out_dir.join("static_fns.c"))
+        .compile("static_fns");
 
-    cc::Build::new()
-        .include(package)
-        .file(output.join("static_fns.c"))
-        .compile("foo");
+    println!("cargo:rerun-if-changed=src/header.h");
 }
