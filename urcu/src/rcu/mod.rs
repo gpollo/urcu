8e6c389b8578eefdda9f99ed660e545ad64c6f59
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2024-10-03 21:01:08 -0400

    urcu: Add rcu_dereference{_mut} functions

--- urcu/src/rcu/mod.rs
+++ urcu/src/rcu/mod.rs
@@ -418,6 +418,34 @@ pub type DefaultContext = flavor::bp::RcuContextBp;
 ))]
 pub type DefaultContext = flavor::qsbr::RcuContextQsbr;
 
+/// Returns an immutable RCU-protected pointer.
+///
+/// > It does not actually dereference the pointer, instead, it protects the pointer
+/// > for later dereferencing. It also executes any needed memory-barrier instructions
+/// > for a given CPU architecture.
+///
+/// #### Safety
+///
+/// * The thread must be inside an RCU critical section.
+pub unsafe fn rcu_dereference<T>(pointer: *const T) -> *const T {
+    // SAFETY: It is safe to cast the pointer to a void*.
+    unsafe { urcu_sys::rcu_dereference(pointer as *mut std::ffi::c_void) as *const T }
+}
+
+/// Returns a mutable RCU-protected pointer.
+///
+/// > It does not actually dereference the pointer, instead, it protects the pointer
+/// > for later dereferencing. It also executes any needed memory-barrier instructions
+/// > for a given CPU architecture.
+///
+/// #### Safety
+///
+/// * The thread must be inside an RCU critical section.
+pub unsafe fn rcu_dereference_mut<T>(pointer: *mut T) -> *mut T {
+    // SAFETY: It is safe to cast the pointer to a void*.
+    unsafe { urcu_sys::rcu_dereference(pointer as *mut std::ffi::c_void) as *mut T }
+}
+
 mod asserts {
     use static_assertions::assert_not_impl_all;
 
