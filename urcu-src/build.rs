aa145b65b63b0a147d0ab64b507ad1b5cae12d9e
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2024-10-03 21:42:11 -0400

    urcu-src: Add -O0, -O1, etc flags when building

--- urcu-src/build.rs
+++ urcu-src/build.rs
@@ -1,8 +1,24 @@
 #[cfg(feature = "static")]
+fn configure_opt(config: &mut autotools::Config) {
+    match std::env::var("OPT_LEVEL") {
+        Err(_) => (),
+        Ok(v) => {
+            if matches!(v.as_str(), "0" | "1" | "2" | "3" | "s") {
+                config.cflag(format!("-O{}", v));
+                config.cxxflag(format!("-O{}", v));
+            }
+        }
+    }
+
+    config.cflag("-g");
+    config.cxxflag("-g");
+}
+
+#[cfg(feature = "static")]
 fn configure_lto(config: &mut autotools::Config) {
     let enable = match std::env::var("CARGO_ENCODED_RUSTFLAGS") {
-        Ok(value) => value.contains("linker-plugin-lto"),
         Err(_) => false,
+        Ok(value) => value.contains("linker-plugin-lto"),
     };
 
     if enable {
@@ -23,6 +39,7 @@ fn main() {
     }
 
     let mut config = autotools::Config::new("vendor");
+    configure_opt(&mut config);
     configure_lto(&mut config);
     config.out_dir(out_dir).reconf("-ivf").build();
 }
