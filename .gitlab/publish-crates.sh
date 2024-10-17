77b84aa60750e8103fc268c0e8f8d51d5ddce04f
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2024-10-17 06:31:41 -0400

    ci: Add crates.io and Gitlab release jobs

---
+++ .gitlab/publish-crates.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+#################
+# cargo helpers #
+#################
+
+function cargo() {
+    >&2 echo " "
+    >&2 echo '$' cargo "$@"
+    >&2 echo " "
+
+    command cargo "$@"
+}
+
+function cargo_login() {
+    cargo login "${CRATES_IO_TOKEN}"
+}
+
+#################
+# crate helpers #
+#################
+
+function crate_name() {
+    local dir="$1"
+    local crate_name
+    local crate_version
+
+    crate_name=$(tq -f "${dir}/Cargo.toml" "package.name")
+    crate_version=$(tq -f "${dir}/Cargo.toml" "package.version")
+
+    echo -n "${crate_name}@${crate_version}"
+}
+
+function crate_released() {
+    local dir="$1"
+
+    cargo -Z unstable-options -C /tmp info "$(crate_name "${dir}")" > /dev/null
+}
+
+function crate_publish() {
+    local dir="$1"
+
+    if ! crate_released "${dir}"; then
+        pushd "${dir}"
+        cargo publish "${@:2}"
+        popd
+    fi
+}
+
+##############
+# entrypoint #
+##############
+
+set -e
+cd "$(dirname "$0")" || exit 1
+
+cargo_login
+crate_publish ../urcu-src
+crate_publish ../urcu-sys
+crate_publish ../urcu-bp-sys
+crate_publish ../urcu-mb-sys
+crate_publish ../urcu-memb-sys
+crate_publish ../urcu-qsbr-sys
+cp ../README.md ../urcu/README.md
+crate_publish ../urcu
---
+++ .gitlab/release-gitlab.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+###############
+# git helpers #
+###############
+
+function git_current_hash() {
+    git rev-parse --short HEAD
+}
+
+#################
+# crate helpers #
+#################
+
+function crate_name() {
+    local dir="$1"
+
+    tq -f "${dir}/Cargo.toml" "package.name"
+}
+
+function crate_version() {
+    local dir="$1"
+
+    tq -f "${dir}/Cargo.toml" "package.version"
+}
+
+##################
+# gitlab helpers #
+##################
+
+function gitlab_release_version() {
+    echo -n "v$(crate_version urcu)"
+}
+
+function gitlab_release_name() {
+    echo -n "Userspace RCU $(gitlab_release_version)"
+}
+
+function gitlab_release_description() {
+    echo "This release contains the following crates."
+    echo ""
+    echo "| Crate                           | Version                            |"
+    echo "|:--------------------------------|:-----------------------------------|"
+    echo "| \`$(crate_name urcu)\`          | \`$(crate_version urcu)\`          |"
+    echo "| \`$(crate_name urcu-bp-sys)\`   | \`$(crate_version urcu-bp-sys)\`   |"
+    echo "| \`$(crate_name urcu-mb-sys)\`   | \`$(crate_version urcu-mb-sys)\`   |"
+    echo "| \`$(crate_name urcu-memb-sys)\` | \`$(crate_version urcu-memb-sys)\` |"
+    echo "| \`$(crate_name urcu-qsbr-sys)\` | \`$(crate_version urcu-qsbr-sys)\` |"
+    echo "| \`$(crate_name urcu-src)\`      | \`$(crate_version urcu-src)\`      |"
+    echo "| \`$(crate_name urcu-sys)\`      | \`$(crate_version urcu-sys)\`      |"
+}
+
+function gitlab_release() {
+    glab config set token "${GITLAB_RELEASE_TOKEN}"
+    glab release create "$(gitlab_release_version)" \
+        --name "$(gitlab_release_name)" \
+        --notes "$(gitlab_release_description)" \
+        --ref "$(git_current_hash)"
+}
+
+##############
+# entrypoint #
+##############
+
+set -e
+cd "$(dirname "$0")/.." || exit 1
+
+gitlab_release
