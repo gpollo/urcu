1ca64ae29d6f708a7df0c176efd2038da7189094
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2024-11-01 23:51:47 -0400

    urcu: Run rustfmt manually on UI tests

--- urcu/tests/ui/list/front-fail-1.rs
+++ urcu/tests/ui/list/front-fail-1.rs
@@ -2,7 +2,7 @@ use urcu::prelude::*;
 
 fn main() {
     let context = DefaultContext::rcu_register().unwrap();
-    
+
     let list = RcuList::<u32>::new();
     let guard = context.rcu_read_lock();
     let front = list.front(&guard);
--- urcu/tests/ui/list/iter-reverse-fail-1.rs
+++ urcu/tests/ui/list/iter-reverse-fail-1.rs
@@ -2,7 +2,7 @@ use urcu::prelude::*;
 
 fn main() {
     let context = DefaultContext::rcu_register().unwrap();
-    
+
     let list = RcuList::<u32>::new();
     let guard = context.rcu_read_lock();
     let mut iter = list.iter_reverse(&guard);
