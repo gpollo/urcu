f23b2419a5bce85f1d72888f051640f3e326f437
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2025-01-15 20:56:54 -0500

    urcu: Update 'hashmap' example

--- urcu/examples/hashmap.rs
+++ urcu/examples/hashmap.rs
@@ -1,7 +1,10 @@
 use std::ops::Range;
 use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
+use std::sync::mpsc::{Receiver, RecvTimeoutError};
 use std::sync::Arc;
+use std::time::Duration;
 
+use clap::Parser;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use urcu::prelude::*;
@@ -34,7 +37,7 @@ impl PublisherThread {
         }
     }
 
-    fn run(self) {
+    fn run(self) -> u128 {
         let mut context = RcuDefaultFlavor::rcu_context_builder()
             .with_read_context()
             .register_thread()
@@ -55,6 +58,8 @@ impl PublisherThread {
         );
 
         self.publisher_count.fetch_sub(1, Ordering::Release);
+
+        node_count
     }
 
     fn publish<C>(&self, keyset: &[u32], context: &mut C) -> u128
@@ -98,7 +103,7 @@ impl ConsumerThread {
         }
     }
 
-    fn run(self) {
+    fn run(self) -> u128 {
         let mut context = RcuDefaultFlavor::rcu_context_builder()
             .with_read_context()
             .with_defer_context()
@@ -123,6 +128,8 @@ impl ConsumerThread {
             "consumed {} nodes in [{}, {}]",
             node_count, self.keyset.start, self.keyset.end
         );
+
+        node_count
     }
 
     fn consume<C>(&self, keyset: &[u32], context: &mut C) -> u128
@@ -137,7 +144,7 @@ impl ConsumerThread {
             context.rcu_quiescent_state();
 
             let guard = context.rcu_read_lock();
-            let item = self.map.remove(&key, &guard);
+            let item = self.map.remove(key, &guard);
             if let Some(ref item) = item {
                 let got = item.value();
                 let expected = key_to_value(*item.key());
@@ -149,60 +156,141 @@ impl ConsumerThread {
             }
 
             drop(guard);
-            item.defer_cleanup(context);
+
+            match node_removed % 3 {
+                0 => item.safe_cleanup(),
+                1 => item.call_cleanup(context),
+                2 => item.defer_cleanup(context),
+                _ => panic!("unexpected"),
+            }
         }
 
         node_removed
     }
 }
 
-fn main() {
-    let map = RcuHashMap::<u32, u64>::new().unwrap();
-    let exit = Arc::new(AtomicBool::new(false));
-    let exit_signal = exit.clone();
-    let publisher_count = Arc::new(AtomicUsize::new(0));
+/// Run a RCU list stress test using multiple threads.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Number of publisher threads.
+    #[arg(short, long, default_value = "4")]
+    publishers: u32,
 
-    ctrlc::set_handler(move || {
-        println!("");
-        exit.store(true, Ordering::Release);
-    })
-    .expect("Error setting Ctrl-C handler");
+    /// Number of consumer threads.
+    #[arg(short, long, default_value = "4")]
+    consumers: u32,
+
+    /// Duration of the test.
+    #[arg(short, long, default_value = "5s", value_parser = humantime::parse_duration)]
+    duration: Duration,
+}
+
+struct ExitHandler(Receiver<()>);
+
+impl ExitHandler {
+    fn configure() -> Self {
+        let (tx, rx) = std::sync::mpsc::channel();
+
+        ctrlc::set_handler(move || {
+            println!();
+            if tx.send(()).is_err() {
+                log::error!("failed to send Ctrl+C signal");
+            }
+        })
+        .expect("Error setting Ctrl-C handler");
+
+        Self(rx)
+    }
+
+    fn wait_for(&self, duration: Duration) {
+        if duration.is_zero() {
+            if self.0.recv().is_err() {
+                log::error!("Ctrl+C handler unexpectedly disconnected");
+            }
+        } else if let Err(RecvTimeoutError::Disconnected) = self.0.recv_timeout(duration) {
+            log::error!("Ctrl+C handler unexpectedly disconnected");
+        }
+    }
+}
+
+fn main() {
+    env_logger::init();
+
+    let args = Args::parse();
 
     std::thread::scope(|scope| {
-        let thread = PublisherThread::new(&exit_signal, &publisher_count, 0..10000, &map);
-        scope.spawn(move || thread.run());
+        let exit = Arc::new(AtomicBool::new(false));
+        let exit_handler = ExitHandler::configure();
+        let map = RcuHashMap::<u32, u64>::new().unwrap();
+        let mut ranges = RangeDistributor::new();
 
-        let thread = PublisherThread::new(&exit_signal, &publisher_count, 5000..15000, &map);
-        scope.spawn(move || thread.run());
+        let publisher_count = Arc::new(AtomicUsize::new(0));
+        let publishers = (0..args.publishers)
+            .map(|_| {
+                let thread = PublisherThread::new(&exit, &publisher_count, ranges.get(), &map);
+                scope.spawn(move || thread.run())
+            })
+            .collect::<Vec<_>>();
 
-        let thread = PublisherThread::new(&exit_signal, &publisher_count, 10000..20000, &map);
-        scope.spawn(move || thread.run());
+        ranges.reset();
 
-        let thread = PublisherThread::new(&exit_signal, &publisher_count, 0..10000, &map);
-        scope.spawn(move || thread.run());
+        let consumers = (0..args.consumers)
+            .map(|_| {
+                let thread = ConsumerThread::new(&publisher_count, ranges.get(), &map);
+                scope.spawn(move || thread.run())
+            })
+            .collect::<Vec<_>>();
 
-        let thread = PublisherThread::new(&exit_signal, &publisher_count, 5000..15000, &map);
-        scope.spawn(move || thread.run());
+        exit_handler.wait_for(args.duration);
+        exit.store(true, Ordering::Release);
 
-        let thread = PublisherThread::new(&exit_signal, &publisher_count, 10000..20000, &map);
-        scope.spawn(move || thread.run());
+        let published_nodes = publishers
+            .into_iter()
+            .map(|handle| handle.join().unwrap())
+            .sum::<u128>();
 
-        let thread = ConsumerThread::new(&publisher_count, 0..10000, &map);
-        scope.spawn(move || thread.run());
+        println!("published a total of {published_nodes} nodes");
 
-        let thread = ConsumerThread::new(&publisher_count, 5000..15000, &map);
-        scope.spawn(move || thread.run());
+        let consumed_nodes = consumers
+            .into_iter()
+            .map(|handle| handle.join().unwrap())
+            .sum::<u128>();
 
-        let thread = ConsumerThread::new(&publisher_count, 10000..20000, &map);
-        scope.spawn(move || thread.run());
+        println!("consumed a total of {consumed_nodes} nodes");
 
-        let thread = ConsumerThread::new(&publisher_count, 0..10000, &map);
-        scope.spawn(move || thread.run());
-
-        let thread = ConsumerThread::new(&publisher_count, 5000..15000, &map);
-        scope.spawn(move || thread.run());
-
-        let thread = ConsumerThread::new(&publisher_count, 10000..20000, &map);
-        scope.spawn(move || thread.run());
+        assert_eq!(published_nodes, consumed_nodes);
     });
+}
+
+struct RangeDistributor {
+    next: usize,
+    ranges: Vec<Range<u32>>,
+}
+
+impl RangeDistributor {
+    fn new() -> Self {
+        Self {
+            next: 0,
+            ranges: vec![
+                0..20000,
+                0..10000,
+                5000..15000,
+                10000..20000,
+                0..10000,
+                5000..15000,
+                10000..20000,
+            ],
+        }
+    }
+
+    fn get(&mut self) -> Range<u32> {
+        let range = self.ranges[self.next].clone();
+        self.next = (self.next + 1) % self.ranges.len();
+        range
+    }
+
+    fn reset(&mut self) {
+        self.next = 0;
+    }
 }
